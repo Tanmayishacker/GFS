9779d06973d55bf21376fa59caae6d00442f5eca
Tanupsd <154455747+Tanupsd@users.noreply.github.com>
2025-01-03 12:36:36 +0530

    improved the bat file

--- Windows.bat
+++ Windows.bat
@@ -5,7 +5,7 @@ if %errorlevel% neq 0 (
     echo Git is not installed. Please install Git and try again.
     exit /b
 )
-
+echo.
 echo "Downloading all submodules..........."
 :: Initialize and update all submodules recursively
 git submodule update --init --recursive
@@ -17,29 +17,53 @@ REM Check if winget is installed
 echo.
 echo Checking for winget installation...
 
+set IsWingetInstalled=false
 winget --version >nul 2>&1
 
-IF %ERRORLEVEL% EQU 0 (
-    echo winget is installed.
-) ELSE (
+if %ERRORLEVEL% EQU 0 (
+    echo Winget is installed.
+    echo.
+    set IsWingetInstalled=true
+) else (
     echo winget is NOT installed.
+    echo.
 )
-
-echo You may get a prompt
-echo.
-setlocal
 
 REM Check if ShiningLight OpenSSL Dev is installed
-echo Checking for ShiningLight OpenSSL Dev installation...
+    echo Checking for ShiningLight OpenSSL Dev installation...
 
-REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /f "ShiningLight.OpenSSL.Dev" >nul 2>&1
+    :: Check if OpenSSL is installed by checking if openssl command is available
+    openssl version >nul 2>&1
 
-IF %ERRORLEVEL% EQU 0 (
-    echo ShiningLight OpenSSL Dev is installed.
-) ELSE (
-    echo ShiningLight OpenSSL Dev is NOT installed.
-    echo you may install OpenSSL from https://slproweb.com/products/Win32OpenSSL.html
-)
+    if %ERRORLEVEL% EQU 0 (
+        echo ShiningLight OpenSSL Dev is installed.
+    ) else (
 
-cmake -G"Visual Studio 2022" -S .
-pause
+        echo ShiningLight OpenSSL Dev is NOT installed.
+        echo.
+
+        if %IsWingetInstalled%=="false" (
+            echo ShiningLight OpenSSL Dev is NOT installed.
+            echo you may install OpenSSL from https://slproweb.com/products/Win32OpenSSL.html
+            exit /b
+        ) 
+        echo "You may get a prompt"
+        echo.
+        winget install -e --id ShiningLight.OpenSSL
+        pause
+    )
+
+    echo "Select the build type from below:"
+    echo "1) Release 2) Debug"
+
+    set /p buildType="Build type:   "
+
+    if %buildType%=="1"(
+        cmake -DCMAKE_BUILD_TYPE=Release -S . -B ./build
+    )
+    else
+    (
+        cmake -DCMAKE_BUILD_TYPE=Debug -S . -B ./build
+    )
+
+exit /b
